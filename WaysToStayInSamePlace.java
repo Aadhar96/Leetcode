ceb1ab802dfa8996e23d43f8e70d963cd63cf2ea
aadhar-sachdeva <aadhar.sachdeva@flipkart.com>
2019-11-27 16:52:37 +0530

    https://leetcode.com/problems/number-of-ways-to-stay-in-the-same-place-after-some-steps

---
+++ WaysToStayInSamePlace.java
@@ -0,0 +1,28 @@
+class Solution {
+    
+    private static int MOD = 1000000007;
+    public int numWays(int steps, int arrLen) {
+        int[][] dp = new int[steps + 1][steps + 1];
+        dp[0][0] = 1;
+        for(int stepsAllowed = 1; stepsAllowed <= steps; stepsAllowed++) {
+            for(int currPos = 0; currPos <= Math.min(stepsAllowed, arrLen - 1) ; currPos++) {
+                if(currPos > stepsAllowed) {
+                    dp[currPos][stepsAllowed] = 0;
+                } else if(currPos == stepsAllowed) {
+                    dp[currPos][stepsAllowed] = 1;
+                } else {
+                    dp[currPos][stepsAllowed] = dp[currPos][stepsAllowed - 1];
+                    
+                    if(currPos > 0) {
+                        dp[currPos][stepsAllowed] = (dp[currPos][stepsAllowed]%MOD + dp[currPos - 1][stepsAllowed - 1]%MOD)%MOD ;
+                    }
+                    
+                    if(currPos < dp.length - 1) {
+                        dp[currPos][stepsAllowed] = (dp[currPos][stepsAllowed]%MOD + dp[currPos + 1][stepsAllowed - 1]%MOD)%MOD ;
+                    }
+                }
+            }
+        }
+        return dp[0][steps];
+    }
+}
